fd0f6f6d2a6614adc0a152db1e763742bda31cba
Nathan Andrade <nathandulino2003@gmail.com>
2020-07-28 22:37:51 -0300

    React App changed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,11 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.linkedin.com/in/nathan-a-1b9436124/">
+        <img src="https://avatars2.githubusercontent.com/u/62774392?s=460&u=5e4000d42d2fc1bdbd3e217911418d2a5ac87602&v=4" alt="Nathan Andrade" style={{ width: "80px", height: "80px" }} />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Desenvolvido por <a href="https://www.linkedin.com/in/nathan-a-1b9436124/"><strong>Nathan de Andrade</strong></a> durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
